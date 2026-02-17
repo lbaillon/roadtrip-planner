a3112f3d80d21c8672482ca46067372a7df0ff84
Alexandre Gaspard <git@alexgaspard.fr>
2026-02-17 17:06:02 +0100

    Fix eslint config

--- packages/shared/eslint.config.js
+++ packages/shared/eslint.config.js
@@ -1,10 +1,21 @@
 import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import { dirname } from 'path'
 import tseslint from 'typescript-eslint'
+import { fileURLToPath } from 'url'
 
-export default tseslint.config(
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+      },
+    },
     rules: {
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-explicit-any': 'warn',
