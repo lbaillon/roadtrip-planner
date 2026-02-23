2f533ba3392193ab4237b7f92dd29e91b49565e9
Alexandre Gaspard <git@alexgaspard.fr>
2026-02-23 11:46:58 +0100

    Fix eslint

--- packages/shared/eslint.config.js
+++ packages/shared/eslint.config.js
@@ -1,22 +1,25 @@
 import js from '@eslint/js'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import { dirname } from 'path'
 import tseslint from 'typescript-eslint'
 import { fileURLToPath } from 'url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-export default defineConfig(globalIgnores(['dist']), {
-  files: ['**/*.{ts}'],
-  extends: [js.configs.recommended, tseslint.configs.recommended],
-  languageOptions: {
-    parserOptions: {
-      tsconfigRootDir: __dirname,
-      project: './tsconfig.json',
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
     },
   },
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-  },
-})
+])
