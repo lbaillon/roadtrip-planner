53a3395785261f53311b5927663da0640fd1595c
Alexandre Gaspard <git@alexgaspard.fr>
2026-02-24 11:21:36 +0100

    Rename Home folder to pages

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './Home/Home'
+import Home from './pages/Home'
 import { LogIn } from './components/LogIn'
 import { SignUp } from './components/SignUp'
 
