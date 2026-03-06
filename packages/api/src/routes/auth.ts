e0f492afb77d26907e1ab79dacdc451c9455de69
Laury Baillon <laury-baillon@hotmail.fr>
2026-03-06 15:56:55 +0100

    add logout endpoint

--- packages/api/src/routes/auth.ts
+++ packages/api/src/routes/auth.ts
@@ -72,7 +72,7 @@ router.post('/refresh', async (req, res) => {
   }
 })
 
-router.post('/logout', async (req, res) => {
+router.post('/logout', async (req, res)=> {
   res.clearCookie('refreshToken')
   res.status(200).json({ message: 'Logged out successfully' })
 })
