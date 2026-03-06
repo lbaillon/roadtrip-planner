85650e5fe3419d3e181b9425027d6614d49624c2
Laury Baillon <laury-baillon@hotmail.fr>
2026-03-06 15:56:55 +0100

    add logout endpoint

--- packages/web/src/contexts/AuthProvider.tsx
+++ packages/web/src/contexts/AuthProvider.tsx
@@ -1,15 +1,24 @@
-import { useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useState } from 'react'
 import { AuthContext } from './AuthContext'
+import { fetchApi } from '#web/lib/api-client'
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [accessToken, setAccessToken] = useState<string | null>(null)
   const queryClient = useQueryClient()
 
-  function logout() {
-    setAccessToken(null)
-    queryClient.clear()
-  }
+  const {mutate:logout} = useMutation({
+    mutationFn: () =>
+      fetchApi(`/api/auth/logout`, {
+        method: 'POST',
+      }),
+    onSuccess: () => {
+      setAccessToken(null)
+      queryClient.clear()
+    },
+  })
+
+
 
   return (
     <AuthContext.Provider value={{ accessToken, setAccessToken, logout }}>
