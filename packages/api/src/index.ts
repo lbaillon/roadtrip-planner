dee1495b9cc7bc08c901646e456c23301078cf04
Alexandre Gaspard <git@alexgaspard.fr>
2026-03-30 17:13:32 +0200

    Set no cache for API responses

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -10,6 +10,10 @@ app.set('trust proxy', 1) // for secure cookies to work correctly behind render.
 app.use(express.json({ limit: '10mb' }))
 app.use(cookieParser())
 
+app.use('/api', (_req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store')
+  next()
+})
 app.use('/api', routes)
 
 app.use(errorHandler) // must be last!
