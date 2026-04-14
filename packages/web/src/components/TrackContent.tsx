0e77bc025356739716368b25d014955794c16395
Laury Baillon <laury-baillon@hotmail.fr>
2026-04-14 16:41:00 +0200

    add elevation chart
    - create new chart to display elevation
    - display new chart in trackContent

--- packages/web/src/components/TrackContent.tsx
+++ packages/web/src/components/TrackContent.tsx
@@ -19,6 +19,7 @@ import { lazy, Suspense, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import styles from './TrackContent.module.css'
 import { TRACK_COLORS } from './MapViewTracksColors'
+import { ElevationChart } from './ElevationChart'
 
 const MapView = lazy(() => import('#web/components/MapView'))
 type WaypointFormData = { name: string; description?: string }
@@ -333,9 +334,11 @@ export default function TrackContent({
               : 'Heure et vitesse personnalisées'}
           </Button>
 
-          <h3 className={styles.humidityPlot}>
-            Taux d'humidité sur le circuit
-          </h3>
+          <h3 className={styles.humidityPlot}>Altitude</h3>
+
+          <ElevationChart coordinates={parsed.coordinates} />
+
+          <h3 className={styles.humidityPlot}>Taux d'humidité</h3>
 
           <HumidityChart
             coordinates={parsed.coordinates}
