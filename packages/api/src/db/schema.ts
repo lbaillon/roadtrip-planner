257f40088d997a8e78f300a624527b487294e760
Laury Baillon <laury-baillon@hotmail.fr>
2026-06-30 15:20:09 +0200

    add schema to share tracks and trips by mail

--- packages/api/src/db/schema.ts
+++ packages/api/src/db/schema.ts
@@ -14,6 +14,7 @@ export const users = sqliteTable('users', {
   username: text('username').notNull().unique(),
   password: text('password').notNull(),
   profilePicture: text('profile-picture'),
+  confirmationKey: text('confirmation_key'),
   createdAt: integer('created_at', { mode: 'timestamp' })
     .notNull()
     .$defaultFn(() => new Date()),
@@ -79,6 +80,54 @@ export const tripTracks = sqliteTable(
   (table) => [primaryKey({ columns: [table.tripId, table.trackId] })]
 )
 
+export const trackSharesEmails = sqliteTable(
+  'track_shares_emails',
+  {
+    trackId: text('fk_track_id')
+      .references(() => tracks.id, { onDelete: 'cascade' })
+      .notNull(),
+    email: text('email').notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.trackId, table.email] })]
+)
+
+export const tripSharesEmails = sqliteTable(
+  'trip_shares_emails',
+  {
+    tripId: text('fk_trip_id')
+      .references(() => trips.id, { onDelete: 'cascade' })
+      .notNull(),
+    email: text('email').notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.tripId, table.email] })]
+)
+
+export const trackSharesUsers = sqliteTable(
+  'track_shares_users',
+  {
+    trackId: text('fk_track_id')
+      .references(() => tracks.id, { onDelete: 'cascade' })
+      .notNull(),
+    userId: text('fk_user_id')
+      .references(() => users.id, { onDelete: 'cascade' })
+      .notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.trackId, table.userId] })]
+)
+
+export const tripSharesUsers = sqliteTable(
+  'trip_shares_users',
+  {
+    tripId: text('fk_trip_id')
+      .references(() => trips.id, { onDelete: 'cascade' })
+      .notNull(),
+    userId: text('fk_user_id')
+      .references(() => users.id, { onDelete: 'cascade' })
+      .notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.tripId, table.userId] })]
+)
+
 // Types
 export type User = typeof users.$inferSelect
 export type NewUser = typeof users.$inferInsert
@@ -86,3 +135,11 @@ export type Track = typeof tracks.$inferSelect
 export type NewTrack = typeof tracks.$inferInsert
 export type Trip = typeof trips.$inferSelect
 export type NewTrip = typeof trips.$inferInsert
+export type TrackShareEmail = typeof trackSharesEmails.$inferSelect
+export type NewTrackShareEmail = typeof trackSharesEmails.$inferInsert
+export type TripShareEmail = typeof tripSharesEmails.$inferSelect
+export type NewTripShareEmail = typeof tripSharesEmails.$inferInsert
+export type TrackShareUser = typeof trackSharesUsers.$inferSelect
+export type NewTrackShareUser = typeof trackSharesUsers.$inferInsert
+export type TripShareUser = typeof tripSharesUsers.$inferSelect
+export type NewTripShareUser = typeof tripSharesUsers.$inferInsert
